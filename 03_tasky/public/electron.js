8c38f898b9abb2f1188a07b4c9e5ef8b301eb8fc
shaydeecoder <shaydeecoder@gmail.com>
2022-01-23 15:44:58 +0100

    Added mainWindow options

--- 03_tasky/public/electron.js
+++ 03_tasky/public/electron.js
@@ -4,6 +4,11 @@ const { app, BrowserWindow } = electron;
 let mainWindow;
 
 app.on("ready", () => {
-  mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({
+    height: 500,
+    width: 300,
+    frame: false,
+    resizable: false,
+  });
   mainWindow.loadURL(`http://localhost:3000`);
 });
